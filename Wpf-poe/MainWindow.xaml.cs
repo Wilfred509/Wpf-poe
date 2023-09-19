4e2628acf8ad0e561b8fb6c31d2a9d5acb93029c
lab_services_student <lab_services_student@lab000000>
2023-09-19 16:07:28 +0000

    Add project files.

---
+++ Wpf-poe/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_poe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        List<library> AppList = new List <library>();
+        private void btn_CLEAR_Click(object sender, RoutedEventArgs e)
+        {
+
+            txtCode.Text = " ";
+            txtName.Text = " ";
+            txtnumber_of_credits.Text = null;
+            txtnumber_of_hours.Text = null;
+            txthours_per_week.Text = null;
+            txtnumber_of_weeks.Text = null;
+            txt_datePicker.Text = null;
+
+        }
+
+        private void btn_ADD_Click(object sender, RoutedEventArgs e)
+        {
+
+            string module_code, module_name;
+            int number_of_credits, hours_per_week, number_of_weeks, number_of_hours,self_study_hours_per_week,self_study_hours_left; DateTime start_date;
+
+
+            module_code= txtCode.Text;
+            module_name=txtName.Text;
+            number_of_credits = Convert.ToInt32(txtnumber_of_credits.Text);
+            number_of_hours = Convert.ToInt32(txtnumber_of_hours.Text);
+            number_of_weeks = Convert.ToInt32(txtnumber_of_weeks.Text);
+            hours_per_week = Convert.ToInt32(txthours_per_week.Text);
+            start_date = Convert.ToDateTime(txt_datePicker.Text);
+
+            self_study_hours_per_week = ((number_of_credits * 10) / number_of_weeks) - hours_per_week;
+
+            self_study_hours_left = (self_study_hours_per_week - number_of_hours);
+
+            library lb = new library(module_code, module_name, number_of_hours, number_of_credits, number_of_weeks, hours_per_week, self_study_hours_left, self_study_hours_per_week, start_date);
+
+            
+
+
+                AppList.Add(lb);
+
+        }
+
+        private void btn_DISPLAY_Click(object sender, RoutedEventArgs e)
+        {
+            var library = from k in AppList select k;
+
+            foreach (library i in AppList)
+            {
+                list_Box.Items.Add(i.studentReport());
+            }
+        }
+
+        private void list_Box_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Wpf-poe/library.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_poe
+{
+    class library
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public int Classhoursperweek { get; set; }
+        public int Numberofcredits { get; set; }
+        public int Numberofweeks { get; set; }
+
+        public string Startdate { get; set; }
+         public int Numberofhours { get; set; }
+        public int Selfstudyhoursperweek { get; set; }
+
+        public int Selfstudyhoursleft { get; set; }
+
+        public library(string module_code,string module_name ,int number_of_credits, int hours_per_week,int number_of_weeks,int number_of_hours,int self_study_hours_per_week,int self_study_hours_left, DateTime start_date)
+        {
+       
+            Name = module_code;
+            Code = module_name;
+            Numberofcredits= number_of_credits;
+            Numberofweeks= number_of_weeks;
+            Classhoursperweek= hours_per_week;
+            DateTime Startdate;
+            Selfstudyhoursperweek = self_study_hours_per_week;
+            Selfstudyhoursleft = self_study_hours_left;
+            Numberofhours= number_of_hours;
+           
+        }
+        public string studentReport ()
+        {
+            return "\n module_code: " + Code + "\t" + "\nmodule_name :" + Name + "\t" + "\nnumber_of_credits :" + Numberofcredits + "\t" + "\nhours_per_week" + Classhoursperweek + "\t" + "\nnumber_of_weeks" + Numberofweeks + "\t" + "\nnumber_of_hours" + Numberofhours + "\nstart_date" + Startdate + "\nself-study_hours_per_week" + Selfstudyhoursperweek + "\t" + "\nself_study_hours_left" + Selfstudyhoursleft + "\t";
+
+        }
+    }
+}
